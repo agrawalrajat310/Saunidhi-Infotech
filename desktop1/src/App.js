2bfbc32158b245e322b6e3e620397481dd07f714
agrawalrajat310 <agrawalrajat03@outlook.com>
2023-07-19 19:02:15 +0530

    desktop app

--- desktop1/src/App.js
+++ desktop1/src/App.js
@@ -1,11 +1,10 @@
 
-import logo from "./App.css";
 import Logo from "./logo.png";
 
 function App() {
   return (
     <div className="logo">
-      <img src={Logo} alt="WEBSITE LOGO" />;
+    
     </div>
   );
 }
