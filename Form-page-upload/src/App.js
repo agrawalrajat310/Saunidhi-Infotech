4f047d5ecc87615c896f13d5ac085192068b6b50
agrawalrajat310 <agrawalrajat03@outlook.com>
2023-09-08 18:49:48 +0530

    upload doc form page

--- Form-page-upload/src/App.js
+++ Form-page-upload/src/App.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import React from "react";
+
+
 
 export default function App() {
   return (
-    <h1 className="text-3xl bg-slate-600 font-bold underline">
-    Hello world!
-  </h1>
-  )
+    
+  );
 }
+ 
