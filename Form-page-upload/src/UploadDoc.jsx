d78b83ed858c303ff3eccb10d6726243e2a5e443
agrawalrajat310 <agrawalrajat03@outlook.com>
2023-09-12 19:56:51 +0530

    upload doc form page

--- Form-page-upload/src/UploadDoc.jsx
+++ Form-page-upload/src/UploadDoc.jsx
@@ -1,79 +1,88 @@
-import React from "react";
 
-const UploadDoc = () => {
-  return (
-    <div>
-      <div>
-        {/* First Block */}
 
-        <h2 className="w-[196px] text-amber-500 text-[22px] font-normal leading-7 mt-20 ml-10">
-          Upload Documents
-        </h2>
-        {/* <hr className="w-[20px]" /> */}
 
-        {/* Instructions block */}
-        <h3 className="w-[323px] text-red-600 text-sm font-normal leading-7 mt-20 ml-10">
-          Instruction for Uploading Image and Documents –
-        </h3>
 
-        <h4 className="w-[389px] text-red-600 text-[13px] font-normal leading-7 mt-3 ml-10">
-          Photo and Documents are required in .jpg or .jpeg image format.
-        </h4>
 
-        {/* Select File button */}
 
-        <h4 className="w-[333px] text-red-600 text-sm font-normal leading-7 ml-10">
-          File size of photo must be within 10kb to 200kb limit.
-        </h4>
 
-        {/* Select File button */}
 
-        <h4 className="w-[351px] text-red-600 text-[13px] font-normal leading-7 ml-10">
-          File size of Documents must be within 600Kb to 2Mb limit.
-        </h4>
 
-        {/* Select File button */}
-      </div>
+// import React from "react";
 
-      {/* Candidate Photo Block  */}
+// const UploadDoc = () => {
+//   return (
+//     <div>
+//       <div>
+//         {/* First Block */}
 
-      <div>
-        <h4 className="w-[317px] text-neutral-800 text-[13px] font-normal leading-[21.12px] mt-16 ml-10">
-          Candidate's Photograph (with 80% face without mask)
-        </h4>
+//         <h2 className="w-[196px] text-amber-500 text-[22px] font-normal leading-7 mt-20 ml-10">
+//           Upload Documents
+//         </h2>
+//         {/* <hr className="w-[20px]" /> */}
 
-        <h5 className="w-[155px] text-indigo-500 text-xs font-normal leading-[21.12px] ml-10">
-          Accepted formats .jpeg, .jpg
-        </h5>
+//         {/* Instructions block */}
+//         <h3 className="w-[323px] text-red-600 text-sm font-normal leading-7 mt-20 ml-10">
+//           Instruction for Uploading Image and Documents –
+//         </h3>
 
-        <h4 className="w-[201px] text-neutral-800 text-xs font-normal leading-[21.12px] mt-8 ml-10">
-          Candidate's Government issued ID{" "}
-        </h4>
+//         <h4 className="w-[389px] text-red-600 text-[13px] font-normal leading-7 mt-3 ml-10">
+//           Photo and Documents are required in .jpg or .jpeg image format.
+//         </h4>
 
-        <h5 className="w-[155px] text-indigo-500 text-xs font-normal leading-[21.12px] ml-10">
-          Accepted formats .jpeg, .jpg
-        </h5>
+//         {/* Select File button */}
 
-        <h4 className="w-[201px] text-neutral-800 text-xs font-normal leading-[21.12px] mt-8 ml-10">
-          Candidate's Last year Marksheet
-        </h4>
+//         <h4 className="w-[333px] text-red-600 text-sm font-normal leading-7 ml-10">
+//           File size of photo must be within 10kb to 200kb limit.
+//         </h4>
 
-        <h5 className="w-[155px] text-indigo-500 text-xs font-normal leading-[21.12px] ml-10">
-          Accepted formats .jpeg, .jpg{" "}
-        </h5>
-      </div>
+//         {/* Select File button */}
 
-      {/* Button for Previous section */}
-      <div>
-        <button className="w-[122px] text-center text-white text-base font-normal leading-tight">
-          Previous Section{" "}
-        </button>
-        <button className="w-[200px] text-center text-white text-base font-normal leading-tight">
-          Proceed to Preview Section
-        </button>
-      </div>
-    </div>
-  );
-};
+//         <h4 className="w-[351px] text-red-600 text-[13px] font-normal leading-7 ml-10">
+//           File size of Documents must be within 600Kb to 2Mb limit.
+//         </h4>
 
-export default UploadDoc;
+//         {/* Select File button */}
+//       </div>
+
+//       {/* Candidate Photo Block  */}
+
+//       <div>
+//         <h4 className="w-[317px] text-neutral-800 text-[13px] font-normal leading-[21.12px] mt-16 ml-10">
+//           Candidate's Photograph (with 80% face without mask)
+//         </h4>
+
+//         <h5 className="w-[155px] text-indigo-500 text-xs font-normal leading-[21.12px] ml-10">
+//           Accepted formats .jpeg, .jpg
+//         </h5>
+
+//         <h4 className="w-[201px] text-neutral-800 text-xs font-normal leading-[21.12px] mt-8 ml-10">
+//           Candidate's Government issued ID{" "}
+//         </h4>
+
+//         <h5 className="w-[155px] text-indigo-500 text-xs font-normal leading-[21.12px] ml-10">
+//           Accepted formats .jpeg, .jpg
+//         </h5>
+
+//         <h4 className="w-[201px] text-neutral-800 text-xs font-normal leading-[21.12px] mt-8 ml-10">
+//           Candidate's Last year Marksheet
+//         </h4>
+
+//         <h5 className="w-[155px] text-indigo-500 text-xs font-normal leading-[21.12px] ml-10">
+//           Accepted formats .jpeg, .jpg{" "}
+//         </h5>
+//       </div>
+
+//       {/* Button for Previous section */}
+//       <div>
+//         <button className="w-[122px] text-center text-white text-base font-normal leading-tight">
+//           Previous Section{" "}
+//         </button>
+//         <button className="w-[200px] text-center text-white text-base font-normal leading-tight">
+//           Proceed to Preview Section
+//         </button>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default UploadDoc;
